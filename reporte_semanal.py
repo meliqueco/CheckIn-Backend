24b50940d933fbb54d464f872c5c7c4815245a45
meliqueco <meliqueco@gmail.com>
2026-03-13 13:04:44 -0600

    Update reporte_semanal.py

--- reporte_semanal.py
+++ reporte_semanal.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 RESEND_API_KEY = os.environ["RESEND_API_KEY"]
-REPORT_EMAIL = "meli_que@yahoo.com"
+REPORT_EMAIL = "meliqueco@gmail.com"
 HORA_ENTRADA = 8  # 8:00am
 
 def get_week_records():
